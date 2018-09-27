4f5c572a2a1d775599ff32376fddc2986f52029f
下森周平 <shuhei@shitamorimac.local>
2018-09-28 01:49:56 +0900

    Initial Commit

---
+++ TrafficSignalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSignalApp
+//
+//  Created by 下森周平 on 2018/09/28.
+//  Copyright © 2018年 Shitamori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
