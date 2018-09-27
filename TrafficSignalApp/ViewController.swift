cd889dc34be67f7eb20c9932f2c1dca31c35902e
下森周平 <shuhei@ShitamoriMac.local>
2018-09-28 03:12:59 +0900

    initial commit

--- TrafficSignalApp/ViewController.swift
+++ TrafficSignalApp/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let redPickerDataList = [1,2]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return redPickerDataList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        return String(redPickerDataList[row])
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        RedPickerView.delegate = self
+        RedPickerView.dataSource = self
     }
 
-
+    @IBOutlet weak var RedPickerView: UIPickerView!
+    
 }
 
